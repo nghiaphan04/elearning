9e59e7a05fedb8bbe2899c10dcebc8fa4ca687c5
nghiaphan04 <nghgiaphan04dev@gmail.com>
2025-02-22 23:57:29 +0700

    fix server connect

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,18 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/build`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-            
-        });
+    mongoose.connection.on("conected",()=>{
+        console.log("Mongodb connected")
+    })
+    await mongoose.connect(`${process.env.MONGO_URL}/lms`);
 
-        console.log("Database Connected");
-    } catch (error) {
-        console.error("Database Connection Error:", error);
-        process.exit(1);
-    }
 };
 
 export default connectDB;
